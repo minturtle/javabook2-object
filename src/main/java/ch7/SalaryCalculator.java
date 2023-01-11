4dcced03ca2250a9140c03aa42453da36b739659
minturtle <minseok0425@kakao.com>
2023-01-11 13:33:47 +0900

    ch10 init

--- src/main/java/ch7/SalaryCalculator.java
+++
@@ -1,9 +0,0 @@
-package ch7;
-
-public class SalaryCalculator {
-
-    public static Long calculate(Long salary, Double taxRate){
-        return Math.round(salary * (100 - taxRate) / 100);
-    }
-
-}
